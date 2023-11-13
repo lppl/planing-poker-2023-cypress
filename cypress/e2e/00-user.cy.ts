d0272ae835a88739e19b804de539edf8a06f3045
Łukasz Pietrek <lpietrek@gmail.com>
2023-11-13 20:09:18 +0100

    Add tests for basic

--- cypress/e2e/00-user.cy.ts
+++ cypress/e2e/00-user.cy.ts
@@ -1,3 +1,42 @@
-describe.skip('Poker room', () => {
-    it('Poker room can be only created', () => {});
+import { itemprop, itemscopeByType, SchemaType } from '../helpers/selectors';
+
+Cypress.config('defaultCommandTimeout', 100);
+
+describe('Players', () => {
+    it('New user receive persistent id', () => {
+        let first_id = '';
+        cy.visit('/')
+            .get(itemscopeByType(SchemaType.User))
+            .then((el) => (first_id = el.attr('itemid')))
+            .visit('/')
+            .get(itemscopeByType(SchemaType.User))
+            .then((el) => el.attr('itemid'))
+            .then((second_id) => {
+                expect(second_id).to.be.equal(first_id);
+            });
+    });
+
+    it('Two different users dont share an Id', () => {
+        let firstId = '';
+        cy.visit('/')
+            .get(itemscopeByType(SchemaType.User))
+            .then((el) => (firstId = el.attr('itemid')))
+            .then(Cypress.session.clearCurrentSessionData)
+            .visit('/')
+            .get(itemscopeByType(SchemaType.User))
+            .then((el) => el.attr('itemid'))
+            .then((second_id) => {
+                expect(second_id).not.to.be.equal(firstId);
+            });
+    });
+
+    it('New user by default is named Anonymous', () => {
+        cy.visit('/')
+            .get(itemscopeByType(SchemaType.User))
+            .find(itemprop('name'))
+            .should('contain', 'Anonymous')
+            .get(itemscopeByType(SchemaType.User))
+            .find(itemprop('type'))
+            .should('contain', 'guest');
+    });
 });
